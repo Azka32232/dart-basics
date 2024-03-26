cbeffce713c5e6ff8873114a8c8c056a00f0624a
Azka32232 <azka32232@gmail.com>
2024-03-25 18:35:08 -0700

    tugas dart-basics

---
+++ azka-dart/azka_dart/bin/azka_dart.dart
@@ -0,0 +1,5 @@
+import 'package:azka_dart/azka_dart.dart' as azka_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${azka_dart.calculate()}!');
+}
---
+++ azka-dart/azka_dart/test/azka_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:azka_dart/azka_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
